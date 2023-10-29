e49995ece27d6660a5e871a336dc7ee81d1de78d
Valeria <jacobivaleria@gmail.com>
2023-10-29 18:58:28 -0300

    Inicializacion del proyecto

--- inicio/static/js/scripts.js
+++ inicio/static/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
+* Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
